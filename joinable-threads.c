0a04137878d6f3945ace927dbc9ff4c36159cbdf
bruce-mig <brucetmig@icloud.com>
2025-01-12 10:24:16 +0200

    first commit

---
+++ joinable-threads.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> /* For working with POSIX threads */
+#include <unistd.h> /* For pause() and sleep() */
+#include <errno.h> /* For using Global variable errno */
+
+// Declare thread handles
+pthread_t pthread2;
+pthread_t pthread3;
+
+void* thread_fn_callback(void *arg) {
+    int th_id = *(int *)arg; // Extract thread ID from argument
+    free(arg); // Free the dynamically allocated memory for thread ID
+    int rc = 0;
+
+    // Simulate some work by the thread
+    while(rc != th_id) {
+        printf("Thread %d is doing some work\n", th_id);
+        sleep(1); // Sleep for 1 second to simulate work
+        rc++;
+    }
+
+    // Allocate memory for the result
+    int *result = calloc(1, sizeof(int));
+    *result = th_id * th_id; // Store the result (square of thread ID)
+
+    return (void *)result; // Return the result as a void pointer
+}
+
+// Function to create a thread
+void thread_create(pthread_t *pthread_handle, int th_id) {
+    pthread_attr_t attr; // Thread attribute
+    pthread_attr_init(&attr);
+
+    int *_th_id = calloc(1, sizeof(int));
+    *_th_id = th_id;
+
+    pthread_attr_setdetachstate(&attr,
+        PTHREAD_CREATE_JOINABLE
+        /* PTHREAD_CREATE_DETACHED */);
+    
+    pthread_create(pthread_handle,
+                     &attr, 
+                     thread_fn_callback,
+                     (void *)_th_id);
+
+ 
+}
+
+
+int main(int argc, char **argv) {
+
+    void *thread_result2;
+    void *thread_result3;
+
+    thread_create(&pthread2, 2);
+    thread_create(&pthread3, 10);
+
+    printf("main fn blocked on pthread join for thread with "
+        "th_id = 2\n");
+
+    pthread_join(pthread2,  &thread_result2);
+
+    if(thread_result2) {
+        printf("Return result from thread 2 = %d\n",
+                *(int *)thread_result2);
+        free(thread_result2);
+        thread_result2 = NULL;
+    }
+
+    printf("main fn blocked on pthread join for thread with "
+        "th_id = 3\n");
+
+    pthread_join(pthread3,  &thread_result3);
+
+    if(thread_result3) {
+        printf("Return result from thread 3 = %d\n",
+                *(int *)thread_result3);
+        free(thread_result3);
+        thread_result3 = NULL;
+    }
+
+    return 0;
+}
