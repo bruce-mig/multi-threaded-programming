2c7a5eeede40ede3d6802b46d225bec7f0facfcb
bruce-mig <brucetmig@icloud.com>
2025-04-20 10:03:27 +0200

    notif implementation

--- thread-basics/notification-chain/notif.c
+++ thread-basics/notification-chain/notif.c
@@ -3,7 +3,8 @@
  *
  *       Filename:  notif.c
  *
- *    Description: This file implements Generaic Notif Chain structures definitions
+ *    Description: This file implements Generaic Notif Chain structures
+ * definitions
  *
  *        Version:  1.0
  *        Created:  16/02/2025 17:17:00 PM
@@ -15,17 +16,16 @@
  * =====================================================================================
  */
 
-#include <stdlib.h>
-#include <memory.h>
-#include <assert.h>
 #include "notif.h"
+#include <assert.h>
+#include <memory.h>
+#include <stdlib.h>
 
-notif_chain_t* nfc_create_new_notif_chain(char *notif_chain_name) {
+notif_chain_t *nfc_create_new_notif_chain(char *notif_chain_name) {
 
    notif_chain_t *nfc = calloc(1, sizeof(notif_chain_t));
    if (notif_chain_name) {
-      strncpy(nfc->nfc_name, notif_chain_name,
-              sizeof(nfc->nfc_name));
+      strncpy(nfc->nfc_name, notif_chain_name, sizeof(nfc->nfc_name));
    }
 
    init_glthread(&nfc->notif_chain_head);
@@ -54,10 +54,8 @@ void nfc_delete_all_nfce(notif_chain_t *nfc) {
    ITERATE_GLTHREAD_END(&nfc->notif_chain_head, curr);
 }
 
-void nfc_invoke_notif_chain(notif_chain_t *nfc,
-                            void *arg, size_t arg_size,
-                            char *key, size_t key_size,
-                            nfc_op_t nfc_op_code) {
+void nfc_invoke_notif_chain(notif_chain_t *nfc, void *arg, size_t arg_size,
+                            char *key, size_t key_size, nfc_op_t nfc_op_code) {
 
    glthread_t *curr;
    notif_chain_elem_t *nfce;
@@ -72,18 +70,20 @@ void nfc_invoke_notif_chain(notif_chain_t *nfc,
 
       nfce = glthread_glue_to_notif_chain_elem(curr);
 
-      if (!(key && key_size &&
-            nfce->is_key_set && (key_size == nfce->key_size))) {
+      // wildcard
+      if (!(key && key_size && nfce->is_key_set &&
+            (key_size == nfce->key_size))) {
 
          nfce->app_cb(arg, arg_size, nfc_op_code, nfce->subs_id);
       } else {
 
+         // keys match
          if (memcmp(key, nfce->key, key_size) == 0) {
 
             nfce->app_cb(arg, arg_size, nfc_op_code, nfce->subs_id);
          }
       }
    }
-   
+
    ITERATE_GLTHREAD_END(&nfc->notif_chain_head, curr);
 }
