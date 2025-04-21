5ad99636d0fb7c5401b2a2954e306223c0f102d6
bruce-mig <brucetmig@icloud.com>
2025-04-21 21:28:26 +0200

    udp sender stdout fmt

--- event-listeners/udp_sender.c
+++ event-listeners/udp_sender.c
@@ -1,9 +1,8 @@
 #include "network_utils.h"
 
-int
-main(int argc, char **argv) {
+int main(int argc, char **argv) {
 
-	printf("Dest = [%s,%d] \n", argv[1], atoi(argv[2]));
-	send_udp_msg(argv[1], atoi(argv[2]), argv[3], strlen( argv[3] ));
-	return 0;
+   printf("Dest = [%s:%d] \n", argv[1], atoi(argv[2]));
+   send_udp_msg(argv[1], atoi(argv[2]), argv[3], strlen(argv[3]));
+   return 0;
 }
